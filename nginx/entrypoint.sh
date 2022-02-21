bcd99d15ec9a504a13ce8a3b90c5260e53508543
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-21 07:15:24 +0100

    nginx v2.0 from source

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 mediaowner=$(ls -ld /html | awk '{print $3}')
 echo "Current /html owner is $mediaowner"
-if [ "$mediaowner" != "www-data" ]
-then
-    chown -R www-data:www-data /html
-fi
-nginx -g "daemon off;"
+/opt/nginx/sbin/nginx -g "daemon off;"
